e132eb5b68a9cedc7c572f1c7a3511293b907519
쿼터파이 <ymsnack@naver.com>
2024-08-04 17:25:58 +0900

    Delete script.js

--- script.js
+++
@@ -1,9 +0,0 @@
-function checkPIN() {
-    const pin = document.getElementById('pin').value;
-    if (pin === '0000') {
-        document.querySelector('.container').classList.add('hidden');
-        document.getElementById('main-page').classList.remove('hidden');
-    } else {
-        alert('Incorrect PIN');
-    }
-}
